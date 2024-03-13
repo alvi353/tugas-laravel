af01ec8b78b46cfbc47e29f1311336fb9935e13d
alvi353 <72272278+alvi353@users.noreply.github.com>
2024-03-13 23:16:24 +0700

    Update web.php

--- web.php
+++ web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -18,16 +18,15 @@ Route::get('/', function () {
 });
 
 Route::get('angka/{nilai}', function ($nilai) {
-        return "$nilai";
+    return "nilai nya = $nilai";
 });
 
-Route::get('Penjumlahan/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
-    return "$nilai1 + $nilai2";
+Route::get('penjumlahan/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
+    return $nilai1 + $nilai2;
 });
 
-Route::get('Pengurangan/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
-    return "$nilai1 - $nilai2";
+Route::get('pengurangan/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
+    return $nilai1 - $nilai2;
 });
 
 ?>
-
